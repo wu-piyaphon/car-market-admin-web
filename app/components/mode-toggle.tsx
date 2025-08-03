7dd1aed9d24a8820f9c38d070cfcb712391819df
Piyaphon Wu <wpiyaphon@gmail.com>
2025-08-03 22:53:45 +0700

    Refactor ModeToggle component: adjust button and icon sizes for improved UI consistency

--- app/components/mode-toggle.tsx
+++ app/components/mode-toggle.tsx
@@ -15,10 +15,10 @@ export function ModeToggle() {
       variant="outline"
       size="icon"
       onClick={handleThemeChange}
-      className="absolute bottom-8 right-8 size-14 rounded-full"
+      className="absolute bottom-8 right-8 size-12 rounded-full"
     >
-      <Sun className=" size-6 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-      <Moon className="size-6 absolute scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <Sun className="size-4 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      <Moon className="size-4 absolute scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
